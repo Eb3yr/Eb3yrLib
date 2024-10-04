8042bb2b96228f1f7b316cdaeb1f9c4646ecca84
Eb3yr <adamkjohnson123@gmail.com>
2024-10-04 12:53:10 +0100

    Created

---
+++ MyLib/Funcs.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Eb3yrLib
+{
+	public static class Funcs
+	{
+		public static T PushFromHalf<T>(T num, T exponent) where T : INumber<T> => PushFrom(num, (T)Convert.ChangeType(0.5d, typeof(T)), exponent);
+		public static T PushFrom<T>(T num, T from, T exponent) where T : INumber<T>
+		{
+			// Consider exponents, hell add a func param that lets the user define custom funcs to apply to the result. Do this in another overload
+			T diff = num - from;
+			bool positive = diff > T.Zero;
+			diff = T.Abs(diff);
+
+
+			throw new NotImplementedException("Go away red squiggly lines");
+		}
+		
+	}
+}
