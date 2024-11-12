31f708ba394214e43de429fd9661df40c9390b7d
Eb3yr <Eb3yr123@gmail.com>
2024-11-12 03:54:16 +0000

    Some basic aerodynamics. Boundary layer profiles mostly unimplemented, some potential flows added.

---
+++ Eb3yrLib/Aerodynamics/Aero.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eb3yrLib.Aerodynamics
+{
+	public static class PlateBLFlowProfiles
+	{
+		public static class Blasius
+		{
+			/// <summary>
+			/// Blasius's proposed parabolic solution to the laminar boundary layer profile.
+			/// </summary>
+			/// <param name="yOverDelta">y/δ, the dimensionless ratio of distance from the surface to boundary layer thickness</param>
+			/// <returns>U / U_infinity, the ratio of flow velocity at y to the freestream flow velocity</returns>
+			public static double Laminar(double yOverDelta)
+			{
+				return 2d * yOverDelta - (yOverDelta * yOverDelta);
+			}
+			// Go find the more suitable name for these ratios
+			public static double ThetaOverX(double reynolds_x) => 0.664d / double.Sqrt(reynolds_x);
+			public static double DeltaStarOverX(double reynolds_x) => 1.771d / double.Sqrt(reynolds_x);
+			public static double LocalSkinFricCoef(double reynolds_x) => ThetaOverX(reynolds_x);
+		}
+	}
+
+	public static class LaminarPlateEquations
+	{
+
+	}
+
+	public static class TurbulentPlateEquations
+	{
+
+	}
+
+	public static class GeneralPlateBLEquations
+	{
+		/// <summary>
+		/// Calculate the total viscous drag coefficient C_F; not to be confused with the local viscous drag coefficient C_f. The same underlying equation as ViscousDragCoefficient(), but with dragPerUnitSpan in place of drag and chordLength in place of surfaceArea.
+		/// </summary>
+		/// <param name="dragPerUnitSpan">Drag per unit span of the plate, D'</param>
+		/// <param name="density">Fluid density, ρ</param>
+		/// <param name="freestreamVelocity">Velocity of the stream flow, U_∞</param>
+		/// <param name="chordLength">Length of the plate, L or c</param>
+		/// <returns>Viscous drag coefficient, C_F</returns>
+		public static double ViscousDragCoefficient_UnitSpan(double dragPerUnitSpan, double density, double freestreamVelocity, double chordLength)
+		{
+			return dragPerUnitSpan / (0.5d * density * freestreamVelocity * freestreamVelocity * chordLength);  // 0.5d in denominator
+		}
+		/// <summary>
+		/// Calculate the total viscous drag coefficient C_F; not to be confused with the local viscous drag coefficient C_f. The same underlying equation as ViscousDragCoefficient_UnitSpan(), but with drag in place of of dragPerUnitSpan and surfaceArea in place of chordLength.
+		/// </summary>
+		/// <param name="drag">Total drag of the plate, D</param>
+		/// <param name="density">Fluid density, ρ</param>
+		/// <param name="freestreamVelocity">Velocity of the stream flow, U_∞</param>
+		/// <param name="surfaceArea">Surface area of the plate, s</param>
+		/// <returns>Viscous drag coefficient, C_F</returns>
+		public static double ViscousDragCoefficient(double drag, double density, double freestreamVelocity, double surfaceArea)
+		{
+			return drag / (0.5d * density * freestreamVelocity * freestreamVelocity * surfaceArea);  // 0.5d in denominator
+		}
+
+		/// <summary>
+		/// Calculates the viscous (skin friction) drag of a plate using fluid density, flow velocity, surface area, and the viscous drag coefficient.
+		/// </summary>
+		/// <param name="density"></param>
+		/// <param name="velocity"></param>
+		/// <param name="surfaceArea"></param>
+		/// <param name="viscousDragCoefficient"></param>
+		/// <returns></returns>
+		public static double Drag(double density, double velocity, double surfaceArea, double viscousDragCoefficient)
+		{
+			return 0.5d * density * velocity * velocity * surfaceArea * viscousDragCoefficient;
+		}
+	}
+}
---
+++ Eb3yrLib/Aerodynamics/PotentialFlows.cs
@@ -0,0 +1,108 @@
+﻿using MathNet.Numerics;
+
+namespace Eb3yrLib.Aerodynamics
+{
+	/// <summary>A streamfunction that accepts cartesian x-z coordinates and returns a magnitude. When adding multiple to one delegate, call .Sum() on that delegate after or else only the result of the last-added streamfunction will be returned</summary>
+	public delegate double StreamFunction(double x, double z);
+
+	/// <summary>Contains stream functions and methods to use them for potential flow theory.</summary>
+	/// <remarks>StreamFunction is a delegate type that represents a function with parameters x and z. Dotnet defines a delegate with multiple delegates added to it to return only the result of the last delegate if they have a return type. It will not return the sum of all of the delegates. For this reason, always call .Sum() on a StreamFunction composed of multiple functions before using it. </remarks>
+	public static class PotentialFlows
+	{
+		const double oneOver2Pi = (0.5 / double.Pi);
+
+		public static StreamFunction Uniform(double Uinf, double alpha = 0) // radians
+		{
+			return (x, z) => Uinf * double.Cos(alpha) * z - Uinf * double.Sin(alpha) * x;
+		}
+
+		/// <summary>Creates a source with the negative of the given strength Q</summary>
+		public static StreamFunction Sink(double Q, double x0 = 0, double z0 = 0) => Source(-Q, x0, z0);
+		public static StreamFunction Source(double Q, double x0 = 0, double z0 = 0)		// Q = area flow rate (volumetric flow rate per unit length)
+		{
+			return (x, z) => Q * oneOver2Pi * double.Atan2(z - z0, x - x0);
+		}
+
+		public static StreamFunction Doublet(double kappa, double x0 = 0, double z0 = 0)
+		{
+			return (x, z) =>
+			{
+				double xDiff = x - x0;
+				double zDiff = z - z0;
+				return -kappa * oneOver2Pi * zDiff / (xDiff * xDiff + zDiff * zDiff);
+			};
+		}
+
+		public static StreamFunction Vortex(double gamma, double x0 = 0, double z0 = 0)		// positive gamma = CW rotation
+		{
+			return (x, z) => gamma * oneOver2Pi * double.Log(Radius(x - x0, z - z0));
+		}
+
+		static double Radius(double x, double z) => double.Sqrt(x * x + z * z);
+
+		public static (double x, double z)[,] CoordGrid(double xMin, double xMax, double zMin, double zMax, double dx, double dz)
+		{
+			int iMax = (int)double.Ceiling((xMax - xMin) / dx);
+			int jMax = (int)double.Ceiling((zMax - zMin) / dz);
+			(double x, double z)[,] arr = new (double x, double z)[iMax + 1, jMax + 1];
+
+			for (int i = 0; i <= iMax; i++)
+				for (int j = 0; j <= jMax; j++)
+					arr[i, j] = (xMin + i * dx, zMin + j * dz);
+
+			return arr;
+		}
+
+		public static (double u, double w)[,] VelocityGrid((double x, double z)[,] meshGrid, StreamFunction func)
+		{
+			var velFunc = GetVelocityFunc(func);
+			var velGrid = new (double u, double w)[meshGrid.GetLength(0), meshGrid.GetLength(1)];
+
+			(double x, double z) val;
+			for (int i = 0; i < meshGrid.GetLength(0); i++)
+				for (int j = 0; j < meshGrid.GetLength(1); j++)
+				{
+					val = meshGrid[i, j];
+					velGrid[i, j] = (velFunc(val.x, val.z));
+				}
+
+			return velGrid;
+		}
+
+		/// <summary>Get the cartesian velocity function from a cartesian stream function</summary>
+		public static Func<double, double, (double u, double w)> GetVelocityFunc(this StreamFunction func)
+		{
+			var _f = new Func<double, double, double>(func);
+			var dfdx = Differentiate.FirstPartialDerivative2Func(_f, 0);
+			var dfdz = Differentiate.FirstPartialDerivative2Func(_f, 1);
+			return (x, z) => (dfdz(x, z), -dfdx(x, z));	// u = dphi/dz, w = -dphi/dx. 
+		}
+
+		public static double[,] StreamGrid((double x, double z)[,] meshGrid, StreamFunction func)
+		{
+			double[,] stream = new double[meshGrid.GetLength(0),meshGrid.GetLength(1)];
+			for (int i = 0; i < meshGrid.GetLength(0); i++)
+			{
+				for (int j = 0; j < meshGrid.GetLength(1); j++)
+				{
+					(double x, double z) val = meshGrid[i, j];
+					stream[i, j] = func(val.x, val.z);
+				}
+			}
+			return stream;
+		}
+
+		/// <summary>Combine the functions within the invocation list of the given StreamFunction such that invoking the returned StreamFunction will return the sum of each function added to that delegate, rather than the result of invoking only the last member of the invocation list.</summary>
+		public static StreamFunction Sum(this StreamFunction sf)
+		{
+			return (double x, double z) =>
+			{
+				double sum = 0;
+				foreach (StreamFunction s in sf.GetInvocationList())
+					sum += s(x, z);
+
+				return sum;
+			};
+		}
+	}
+}
