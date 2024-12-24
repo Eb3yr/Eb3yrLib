dcb63eb6775ff93cdfcdb1910a394e0a462bb7b8
Eb3yr <Eb3yr123@gmail.com>
2024-12-24 02:51:27 +0000

    Minor documentation changes

--- Eb3yrLib/Collections/Array2D.cs
+++ Eb3yrLib/Collections/Array2D.cs
@@ -4,26 +4,32 @@ using System.Runtime.CompilerServices;
 namespace Eb3yrLib.Collections
 {
 	/// <summary>A 2D array implementation using a 1D backing array</summary>
-	/// <typeparam name="T"></typeparam>
-	/// <param name="lengthX">Length of the first dimension</param>
-	/// <param name="lengthY">Length of the second dimension</param>
 	/// <remarks> y dimension elements for a given x index are contiguous in the backing array. Accessing all y elements for a given x index performs like a jagged or 1D array, but accessing all of x for a given y performs moderately worse than a multidimensional array. Ideally enumerate manually. While it implements IEnumerable<T>, avoid LINQ where unecessary due to significant overhead on fast operations</remarks>
 	public readonly struct Array2D<T> : IReadOnlyList<T>   // ReadOnly to avoid implementing everything else. It's still mutable (:
 	{
+		/// <param name="lengthX">Length of the first dimension</param>
+		/// <param name="lengthY">Length of the second dimension</param>
 		public Array2D(int lengthX, int lengthY)
 		{
 			values = new T[lengthX * lengthY];
 			xBound = lengthX - 1;
 			yBound = lengthY - 1;
 		}
+
+		/// <param name="lengthX">Length of the first dimension</param>
+		/// <param name="lengthY">Length of the second dimension</param>
+		/// <param name="defaultValue">Default value to populate the array with</param>
 		public Array2D(int lengthX, int lengthY, T defaultValue) : this(lengthX, lengthY)
 		{
 			for (int i = 0; i < values.Length; i++)
 				values[i] = defaultValue;
 		}
 
+		/// <param name="length">Length of each dimension</param>
 		public Array2D(int length) : this(length, length) { }
 
+		/// <param name="length">Length of each dimension</param>
+		/// <param name="defaultValue">Default value to populate the array with</param>
 		public Array2D(int length, T defaultValue) : this(length, length, defaultValue) { }
 
 		private readonly int xBound;
@@ -36,12 +42,12 @@ namespace Eb3yrLib.Collections
 
 		public T this[int x, int y]
 		{
-			// No bounds checks for a dramatic increase in performance. Went from ~30% slower than a multidim array to within error of jagged, and only ~3% slower than 1D access
 			get => values[GetIndex(x, y)];
 			set => values[GetIndex(x, y)] = value;
 		}
 
-		/// <summary>Returns the length of the array in the given dimension. Argument of -1 gives the product of each dimension's lengths</summary>
+		/// <summary>Returns the length of the array in the given dimension</summary>
+		/// <param name="dimension">Which dimension to get the length of. -1 will give the product of each dimension's lengths (the length of the backing 1D array)</param>
 		public int Length(int dimension = -1)
 		{
 			return dimension switch
@@ -55,7 +61,7 @@ namespace Eb3yrLib.Collections
 
 		/// <summary>Gets the index by multiplying x by xBound and adding y. Means that y elements for a given x are contiguous, quicker in a nested for loop where the outer loop is x. Slightly slower than a multidimensional array otherwise. </summary>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private int GetIndex(int x, int y) => xBound * x + y;   // multiply by x instead of y so that all y vals are adjacent. Expecting nested for, with y inside the x loop. Might speed up.
+		private int GetIndex(int x, int y) => xBound * x + y;   // multiply by x instead of y so that all y vals are adjacent for ideal performance in nested for loops with y on the inside
 
 		public IEnumerator<T> GetEnumerator() => values.AsEnumerable().GetEnumerator();
 
