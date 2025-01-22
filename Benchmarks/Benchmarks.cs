b6003208a0431fa884d2d0148a9983b3b72da73d
Eb3yr <Eb3yr123@gmail.com>
2025-01-22 04:53:31 +0000

    Quaternion.AsVector4() benchmarks

--- Benchmarks/Benchmarks.cs
+++ Benchmarks/Benchmarks.cs
@@ -2,128 +2,154 @@
 using BenchmarkDotNet.Attributes;
 using Eb3yrLib;
 using Eb3yrLib.Extensions;
-using Eb3yrLib.Maths;
+using Eb3yrLib.Mathematics;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace BenchmarkProj
 {
 	[MemoryDiagnoser(true)]
 	public class Benchmarks
 	{
-		List<int> ten;
-		List<int> hundred;
-		List<int> thousand;
-		List<int> hundredThousand;
-
-
+		Quaternion q = new();
+		Vector4 v4;
 		public Benchmarks()
 		{
-			ten = Enumerable.Range(0, 10).ToList();
-			hundred = Enumerable.Range(0, 100).ToList();
-			thousand = Enumerable.Range(0, 1000).ToList();
-			hundredThousand = Enumerable.Range(0, 100_000).ToList();
+
 		}
 
-		[Benchmark]
-		public void RemoveCol10()
+		[Benchmark(Baseline = true)]
+		public void AsVector4()
 		{
-			_ = RemoveAtCol(ten, 1);
-		}
-
-		[Benchmark]
-		public void RemoveEnumerable10()
-		{
-			_ =RemoveAtEnumerable(ten, 1).ToList();
-		}
-
-		[Benchmark]
-		public void RemoveWhere10()
-		{
-			_ = RemoveAtWhere(ten, 1).ToList();
-		}
-
-		[Benchmark]
-		public void RemoveCol100()
-		{
-			_ = RemoveAtCol(hundred, 1).ToList();
-		}
-
-		[Benchmark]
-		public void RemoveEnumerable100()
-		{
-			_ = RemoveAtEnumerable(hundred, 1).ToList();
-		}
-
-		[Benchmark]
-		public void RemoveWhere100()
-		{
-			_ = RemoveAtWhere(hundred, 1).ToList();
-		}
-
-		[Benchmark]
-		public void RemoveCol1000()
-		{
-			_ = RemoveAtCol(thousand, 1);
-		}
-		
-		[Benchmark]
-		public void RemoveEnumerable1000()
-		{
-			_ = RemoveAtEnumerable(thousand, 1).ToList();
-		}
-		
-		[Benchmark]
-		public void RemoveWhere1000()
-		{
-			_ = RemoveAtWhere(thousand, 1).ToList();
-		}
-		
-		[Benchmark]
-		public void RemoveCol100000()
-		{
-			_ = RemoveAtCol(hundredThousand, 1);
-		}
-		
-		[Benchmark]
-		public void RemoveEnumerable100000()
-		{
-			_ = RemoveAtEnumerable(hundredThousand, 1).ToList();
-		}
-		
-		[Benchmark]
-		public void RemoveWhere100000()
-		{
-			_ = RemoveAtWhere(hundredThousand, 1).ToList();
-		}
-
-
-		public static List<T> RemoveAtCol<T>(List<T> l, int i)
-		{
-			return [.. l[..i], .. l[(i + 1)..]];
-		}
-
-		public static IEnumerable<T> RemoveAtEnumerable<T>(IEnumerable<T> en, int i)
-		{
-			var e = en.GetEnumerator();
-			int c = 0;
-			while (e.MoveNext())
+			for (int i = 0; i < 1_000; i++)
 			{
-				if (c == i)
-					continue;
-
-				c++;
-				yield return e.Current;
+				v4 = q.AsVector4();
 			}
 		}
 
-		public static IEnumerable<T> RemoveAtWhere<T>(IEnumerable<T> en, int i)
+		[Benchmark]
+		public void CloneOfAsVector4AggressiveInlining()
 		{
-			return en.Where((_, index) => index != i);
+			for (int i = 0; i < 1_000; i++)
+			{
+				v4 = q.AsVector4AggressiveInlining();
+			}
+		}
+
+		[Benchmark]
+		public void CloneOfAsVector4NoInlining()
+		{
+			for (int i = 0; i < 1_000; i++)
+			{
+				v4 = q.AsVectorNoInlining();
+			}
+		}
+
+		[Benchmark]
+		public void CloneOfAsVector4NoAttribute()
+		{
+			for (int i = 0; i < 1_000; i++)
+			{
+				v4 = q.AsVector4NoAttribute();
+			}
+		}
+
+		[Benchmark]
+		public unsafe void MemoryMarshalReadWrite()
+		{
+			Span<byte> s = new byte[sizeof(Quaternion)];
+			for (int i = 0; i < 1_000; i++)
+			{
+				MemoryMarshal.Write(s, q);
+				v4 = MemoryMarshal.Read<Vector4>(s);
+			}
+		}
+
+		[Benchmark]
+		public unsafe void MemoryMarshalReadWriteStackAlloc()
+		{
+			Span<byte> s = stackalloc byte[sizeof(Quaternion)];
+			for (int i = 0; i < 1_000; i++)
+			{
+				MemoryMarshal.Write(s, q);
+				v4 = MemoryMarshal.Read<Vector4>(s);
+			}
+		}
+
+		[Benchmark]
+		public void CopyFields()
+		{
+			for (int i = 0; i < 1_000; i++)
+			{
+				v4 = new Vector4(q.X, q.Y, q.Z, q.W);
+			}
+		}
+
+		[Benchmark]
+		public unsafe void UnsafeBitCast()	// AsVector4 uses this
+		{
+			for (int i = 0; i < 1_000; i++)
+			{
+				v4 = Unsafe.BitCast<Quaternion, Vector4>(q);
+			}
+		}
+
+		[Benchmark]
+		public unsafe void UnsafeReadUnaligned()	// Used by Unsafe.BitCast, along with a check for sizeof(TFrom) == sizeof(TTo)
+		{
+			for (int i = 0; i < 1_000; i++)
+			{
+				v4 = Unsafe.ReadUnaligned<Vector4>(ref Unsafe.As<Quaternion, byte>(ref q));
+			}
+		}
+
+		[Benchmark]
+		public unsafe void UnsafeRead()
+		{
+			for (int i = 0; i < 1_000; i++)
+			{
+				v4 = Unsafe.Read<Vector4>(Unsafe.AsPointer(ref q));
+			}
+		}
+	}
+
+	// Clones of System.Numerics.Quaternion.Extensions Vector.AsVector4(this Quaternion value), without its Intrinsic 
+	public static class VectorExt
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static Vector4 AsVector4AggressiveInlining(this Quaternion value)
+		{
+#if MONO
+            return Unsafe.As<Quaternion, Vector4>(ref value);
+#else
+			return Unsafe.BitCast<Quaternion, Vector4>(value);
+#endif
+		}
+
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		public static Vector4 AsVectorNoInlining(this Quaternion value)
+		{
+#if MONO
+            return Unsafe.As<Quaternion, Vector4>(ref value);
+#else
+			return Unsafe.BitCast<Quaternion, Vector4>(value);
+#endif
+		}
+
+		public static Vector4 AsVector4NoAttribute(this Quaternion value)
+		{
+#if MONO
+            return Unsafe.As<Quaternion, Vector4>(ref value);
+#else
+			return Unsafe.BitCast<Quaternion, Vector4>(value);
+#endif
 		}
 	}
 }
