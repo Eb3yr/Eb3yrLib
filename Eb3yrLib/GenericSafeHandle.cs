0215c1bdfcf12efe81110adabbc3d9eca15bf802
Eb3yr <Eb3yr123@gmail.com>
2025-10-27 17:59:23 +0000

    Make release delegate private.

--- Eb3yrLib/GenericSafeHandle.cs
+++ Eb3yrLib/GenericSafeHandle.cs
@@ -4,7 +4,7 @@
 	/// <remarks>Pointers cannot be marshalled to this handle because a delegate to delete the unmanaged resource must be provided upon creation.</remarks>
 	public sealed class GenericSafeHandle : Microsoft.Win32.SafeHandles.SafeHandleZeroOrMinusOneIsInvalid
 	{
-		public readonly Action<nint> _releaseDelegate;
+		readonly Action<nint> _releaseDelegate;
 
 		// While documentation advises us to implement a parameterless constructor so that a native method returning a pointer can be marshalled as a SafeHandle,
 		// we deliberately do not implement it so as to cause a compile-time error, as we require a releaseDelegate to be provided upon creation.
