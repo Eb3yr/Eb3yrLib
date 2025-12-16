d87053575f96f827b1422162b7b4c43a74f76488
Eb3yr <Eb3yr123@gmail.com>
2025-12-16 16:48:31 +0000

    Add ToByteMemoryManager. Allows converting Memory<T> where T is unmanaged into a Memory<byte> without copying.

---
+++ Eb3yrLib/ToByteMemoryManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Buffers;
+using System.Runtime.InteropServices;
+
+namespace Eb3yrLib
+{
+	public sealed class ToByteMemoryManager<T>(Memory<T> memory) : MemoryManager<byte> where T : unmanaged
+	{
+		public ToByteMemoryManager(T[] array) : this(array.AsMemory()) { }
+		readonly Memory<T> _memory = memory;
+		public override Span<byte> GetSpan() => MemoryMarshal.Cast<T, byte>(_memory.Span);
+		public override MemoryHandle Pin(int elementIndex = 0) => throw new NotImplementedException();
+		public override void Unpin() => throw new NotImplementedException();
+		protected override void Dispose(bool disposing) { }
+	}
+}
