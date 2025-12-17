944bd66962de022d44aac6af4da1657efb871a80
Eb3yr <Eb3yr123@gmail.com>
2025-12-17 01:17:04 +0000

    Remove silly useAsync=false test

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -4,7 +4,6 @@ using static System.Console;
 FindSetOfInts.Run();
 FindSetOfInts.RunSyncTask();
 await FindSetOfInts.RunAsync();
-await FindSetOfInts.RunAsyncFalse();
 
 WriteLine("Done");
 ReadLine();
